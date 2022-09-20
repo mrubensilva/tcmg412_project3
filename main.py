2d3a281374e574ba64708085f770faa3cabc88db
edgaar011 <55937776+edgaar011@users.noreply.github.com>
2022-09-20 12:44:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -62,17 +62,52 @@ print("\nInspecting file now...")
 with open(local_log, "r") as fp:
   lines = len(fp.readlines())
   print("\nTotal requests made in time period represented in log file:", lines)
-    
+ fp.close
+
+totalLogsSix = 0 #initialize counter for the six months
+f = open(local_log, "r")
+if f.mode == 'r':
+    fl = f.readlines()
+    for x in fl:
+      if (x[0] == 'l') and (x[10] == '[') : #if the line starts with l then it is a different format than rtemote
+        temp_date =  x[11:22] #we dissect these elements since we only want to see day month and year
+        if temp_date[7:] == '1995': #looking for th eyear 1995 only filter
+          if temp_date[3:6] == 'Jun' or temp_date[3:6] == 'Jul' or temp_date[3:6] == 'Aug' or temp_date[3:6] == 'Sep' or temp_date[3:6] == 'May': #if the month is from May-Sept it auto qualifies
+            totalLogsSix = totalLogsSix + 1
+          elif temp_date[3:6] == 'Apr': #if it is april we need to check if it is the 11th and after
+              if int(temp_date[0:2]) >= 11:
+                totalLogsSix = totalLogsSix + 1
+                #print(temp_date)
+          elif temp_date[3:6] == 'Oct': #if it is october we check if it is the 11th or before
+            if int(temp_date[0:2]) <= 11:
+                totalLogsSix = totalLogsSix + 1
+
+      #the same as above but if line starts with remote
+      if (x[0] == 'r') and (x[11] == '[') :
+        temp_date =  x[12:23]
+        if temp_date[7:] == '1995': #looking for th eyear 1995 only filter
+          if temp_date[3:6] == 'Jun' or temp_date[3:6] == 'Jul' or temp_date[3:6] == 'Aug' or temp_date[3:6] == 'Sep' or temp_date[3:6] == 'May': #if the month is from May-Sept it auto qualifies
+            totalLogsSix = totalLogsSix + 1
+          elif temp_date[3:6] == 'Apr':
+              if int(temp_date[0:2]) >= 11:
+                totalLogsSix = totalLogsSix + 1
+          elif temp_date[3:6] == 'Oct':
+            if int(temp_date[0:2]) <= 11:
+                totalLogsSix = totalLogsSix + 1
+
+print("The total logs from the last 6 months are", totalLogsSix)            
+f.close
+
 # Read local log file and count requests in 6 months
-for line in open(local_log):
-  fp.countif
+# for line in open(local_log):
+#   fp.countif
 
 # Read local log file and count requests in 6 months
 # This includes all entries made after 11/Apr/1995
-def file_len(local_log):
-    with open(local_log) as f:
-        for line in f:
-            if line.strip() == "{remote - - [11/Apr/1995:00:00:16 -0600] "GET 6721.gif HTTP/1.0" 200 1277}":
-                break
-        return sum(1 for line in f)
+# def file_len(local_log):
+#     with open(local_log) as f:
+#         for line in f:
+#             if line.strip() == "{remote - - [11/Apr/1995:00:00:16 -0600] "GET 6721.gif HTTP/1.0" 200 1277}":
+#                 break
+#         return sum(1 for line in f)
 
